d669b761fc3f247996bfcdacedad0e6b0c9ed83e
Jeremiah Almazar <jemalmazar@gmail.com>
2026-05-12 19:36:37 -0700

    Build Part component and refactor Content component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,6 +1,16 @@
 const Header = (props) => <h1>{props.course}</h1>
 
-const Content = (props) => <p>{props.part} {props.exercises}</p>
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part[0]} exercises={props.exercises[0]}/>
+      <Part part={props.part[1]} exercises={props.exercises[1]}/>
+      <Part part={props.part[2]} exercises={props.exercises[2]}/>
+    </div>
+  )
+}
+
+const Part = (props) => <p>{props.part} {props.exercises}</p>
 
 const Total = (props) => <p>Number of exercises {props.total}</p>
 
@@ -12,13 +22,11 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-
+  
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]}/>
       <Total total={exercises1 + exercises2 + exercises3}/>
     </div>
   )
