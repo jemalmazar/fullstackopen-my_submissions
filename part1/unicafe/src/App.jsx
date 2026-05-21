cc87d32ff70d9414e06e4db9bbcae1a7463f67b2
Jeremiah Almazar <jemalmazar@gmail.com>
2026-05-21 00:04:42 -0700

    Build Button component for unicafe app

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 
 const Header = (props) => <h1>{props.header}</h1>
 
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button> 
+
 const App = () => {
   // save clicks of each button to its own state
   const header = 'give feedback'
@@ -9,9 +11,16 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
   return (
     <div>
       <Header header={header} />
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNeutralClick} text='neutral' />
+      <Button onClick={handleBadClick} text='bad' />
     </div>
   )
 }
