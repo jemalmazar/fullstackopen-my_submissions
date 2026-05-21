c34176f8466ab5138f470c22b4d8d4539c9d2fd3
Jeremiah Almazar <jemalmazar@gmail.com>
2026-05-20 21:53:59 -0700

    Build Header component for unicafe app

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 
+const Header = (props) => <h1>{props.header}</h1>
+
 const App = () => {
   // save clicks of each button to its own state
+  const header = 'give feedback'
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
     <div>
-      code here
+      <Header header={header} />
     </div>
   )
 }
