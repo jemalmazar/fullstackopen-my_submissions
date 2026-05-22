81b7d0aab63cc2128d5f946445f3d047dcdd33ae
Jeremiah Almazar <jemalmazar@gmail.com>
2026-05-22 00:12:51 -0700

    Build Statistics component for unicafe app

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,11 +2,25 @@ import { useState } from 'react'
 
 const Header = (props) => <h1>{props.header}</h1>
 
+const Statistics = ({header, stats}) => {
+  return (
+    <div>
+      <h2>{header}</h2>
+      <StatisticLine text='good' value={stats[0]}/>
+      <StatisticLine text='neutral' value={stats[1]}/>
+      <StatisticLine text='bad' value={stats[2]}/>
+    </div>
+  )
+}
+
+const StatisticLine = ({text, value}) => <p>{text} {value}</p>
+
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button> 
 
 const App = () => {
   // save clicks of each button to its own state
   const header = 'give feedback'
+  const statistics = 'statistics'
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -21,6 +35,7 @@ const App = () => {
       <Button onClick={handleGoodClick} text='good' />
       <Button onClick={handleNeutralClick} text='neutral' />
       <Button onClick={handleBadClick} text='bad' />
+      <Statistics header={statistics} stats={[good, neutral, bad]}/>
     </div>
   )
 }
