adf2655ac7fc43bb1d98f71977335ae9af636b07
Jeremiah Almazar <jemalmazar@gmail.com>
2026-05-23 00:12:09 -0700

    Add total, average, and positive stats to Statistics component for unicafe app

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,9 +6,12 @@ const Statistics = ({header, stats}) => {
   return (
     <div>
       <h2>{header}</h2>
-      <StatisticLine text='good' value={stats[0]}/>
-      <StatisticLine text='neutral' value={stats[1]}/>
-      <StatisticLine text='bad' value={stats[2]}/>
+      <StatisticLine text='good:' value={stats[0]}/>
+      <StatisticLine text='neutral:' value={stats[1]}/>
+      <StatisticLine text='bad:' value={stats[2]}/>
+      <StatisticLine text='total:' value={stats[3]}/>
+      <StatisticLine text='average:' value={stats[4]}/>
+      <StatisticLine text='positive:' value={stats[5]}/>
     </div>
   )
 }
@@ -24,10 +27,22 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [feedbackScore, setScore] = useState(0)
+  const feedbackTotal = good + neutral + bad
+  const avgFeedbackScore = feedbackScore/feedbackTotal
+  const positive = `${(good/feedbackTotal) * 100} %`
 
-  const handleGoodClick = () => setGood(good + 1)
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setScore(feedbackScore + 1)
+  }
+
   const handleNeutralClick = () => setNeutral(neutral + 1)
-  const handleBadClick = () => setBad(bad + 1)
+  
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setScore(feedbackScore - 1)
+  }  
 
   return (
     <div>
@@ -35,7 +50,7 @@ const App = () => {
       <Button onClick={handleGoodClick} text='good' />
       <Button onClick={handleNeutralClick} text='neutral' />
       <Button onClick={handleBadClick} text='bad' />
-      <Statistics header={statistics} stats={[good, neutral, bad]}/>
+      <Statistics header={statistics} stats={[good, neutral, bad, feedbackTotal, avgFeedbackScore, positive]}/>
     </div>
   )
 }
