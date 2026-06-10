b38d216c198720579f9c43ff4c295e685447b880
Jeremiah Almazar <jemalmazar@gmail.com>
2026-06-09 19:31:27 -0700

    Build Numbers and Person components to display entries and add functionality to add new entries for phonebook app

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,51 @@
 import { useState } from 'react'
 
+const Numbers = ({data}) => {
+  return (
+    <div>
+      <h2>Numbers</h2>
+      <ul>
+        {data.map(person => <Person key={person.name} person={person}/>)}
+      </ul>
+    </div>
+  )
+}
+
+const Person = (props) => <li>{props.person.name}</li>
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addEntry = (event) => {
+    event.preventDefault()
+    const person = {
+      name: newName
+    }
+
+    setPersons(persons.concat(person))
+    setNewName('')  
+  }
+
+  const handleNameInputChange = (event) => {
+    setNewName(event.target.value)    
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form name="entry_form" onSubmit={addEntry}>
         <div>
-          name: <input />
+          <label htmlFor="name_input">Name:</label>
+          <input name="name_input" value={newName} onChange={handleNameInputChange} required />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      ...
+      <Numbers data={persons}/> 
     </div>
   )
 }
