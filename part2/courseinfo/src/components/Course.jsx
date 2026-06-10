4aaffac8c210cc07a9e7fb053ea2306bb509ac71
Jeremiah Almazar <jemalmazar@gmail.com>
2026-06-09 19:38:18 -0700

    Fix return statement syntax in Course, Part, and Total components of part2/courseinfo app

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,27 +1,33 @@
-const Course = ({course}) => (
-  <div>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total total={course.parts} />
-  </div>
-)
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total total={course.parts} />
+    </div>
+  )
+}
 
 const Header = (props) => <h2>{props.course}</h2>
 
 const Content = ({parts}) => parts.map(part => <Part key={part.id} part={part} />)
 
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
 
-const Total = (props) => (
-  <p><b>
-    total of {props.total.reduce((acc, obj) => {
-      return acc + obj.exercises
-    }, 0)} exercises
-  </b></p>
-)
+const Total = (props) => {
+  return (
+    <p><b>
+      total of {props.total.reduce((acc, obj) => {
+        return acc + obj.exercises
+      }, 0)} exercises
+    </b></p>
+  )
+}
 
 export default Course
