814d356e0fe90f3b314c2e6da855e0c8d2f5b302
Jeremiah Almazar <jemalmazar@gmail.com>
2026-06-11 01:46:07 -0700

    Add phone number <input /> and refactor App() component to handle multiple inputs for part2/phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -13,8 +13,26 @@ const Form = (props) => {
   return (
     <form id="entry_form" onSubmit={props.onSubmit}>
       <div>
-        <label htmlFor="fullname">Name:</label>
-        <input type="text" id="fullname" value={props.value} onChange={props.onChange} required />
+        <label htmlFor="name">Name:</label>
+        <input 
+          type="text"
+          id="name"
+          value={props.value.name}
+          onChange={props.onChange}
+          placeholder="Enter Full Name"
+          autoComplete="on"
+          required />
+      </div>
+      <div>
+        <label htmlFor="number">Number:</label>
+        <input 
+            type="text" 
+            id="number"
+            value={props.value.number}
+            onChange={props.onChange}
+            placeholder="Enter Phone Number"
+            required
+          />
       </div>
       <div>
         <button type="submit">add</button>
@@ -34,35 +52,48 @@ const Numbers = ({header, data}) => {
   )
 }
 
-const Person = (props) => <li>{props.person.name}</li>
+const Person = (props) => <li>{props.person.name} 📞 {props.person.number}</li>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
-  const [newName, setNewName] = useState('')
+  // initial state for adapted from https://medium.com/@amitsharma_24072/handling-multiple-inputs-in-reactjs-best-practices-for-react-js-input-forms-9b973f4beb7e
+  const [newName, setNewName] = useState({
+    name: "",
+    number: ""
+  })
 
-  const addEntry = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault()
     const entry = {
-      name: newName
+      name: newName.name,
+      number: newName.number
     }
-    const isDuplicate = persons.find(person => person.name === newName)
+    const isDuplicate = persons.find(person => person.name === newName.name)
 
     isDuplicate
-      ? alert(`${newName} has already been added to the phonebook`)
+      ? alert(`${newName.name} has already been added to the phonebook`)
       : setPersons(persons.concat(entry))
       
-    setNewName('')
+    setNewName({
+      name: "",
+      number: ""
+    })
   }
 
-  const handleNameInputChange = (event) => {
-    setNewName(event.target.value)    
+  const handleFormInputChange = (event) => {
+    // multiple input handling adapted from https://www.w3schools.com/react/react_forms_multiple_inputs.asp
+    const { id, value } = event.target    
+    setNewName(previousState => ({...previousState, [id]: value}))    
   }
 
   return (
     <div>
-      <Phonebook header='Phonebook' onSubmit={addEntry} onChange={handleNameInputChange} value={newName}/>
+      <Phonebook header='Phonebook' onSubmit={onSubmit} onChange={handleFormInputChange} value={newName}/>
       <Numbers header='Numbers' data={persons}/> 
     </div>
   )
