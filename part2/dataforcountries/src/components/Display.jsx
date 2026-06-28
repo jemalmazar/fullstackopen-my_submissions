c84d91a8c121c8f7608a43e59fcd7fe3629a3faf
Jeremiah Almazar <jemalmazar@gmail.com>
2026-06-28 01:00:00 -0700

    Refactor if statements in Display.jsx of part2/dataforcountries

--- part2/dataforcountries/src/components/Display.jsx
+++ part2/dataforcountries/src/components/Display.jsx
@@ -4,17 +4,12 @@ import Item from "./Item"
 const Display = ({ data, search, onClick }) => {
   const countriesFilter = data.filter(country => country.name.common.toLowerCase().includes(search.toLowerCase()))
 
-  if (search.length === 0) {
-    return null
-  }
-
-  if (countriesFilter.length > 10) {
-    return <p>Too many matches, refine search</p>
-  }
-  
-  if (countriesFilter.length === 1) {
-    return countriesFilter.map(country => <Country key={ country.cca3 } country={ country } />)
-  }
+  if (search.length === 0) return null
+    else if (countriesFilter.length > 10) return <p>Too many matches, refine search</p>
+    else if (countriesFilter.length === 1) {
+      return countriesFilter.map(country => <Country key={ country.cca3 } country={ country } />)
+    }
+    else if (countriesFilter.length === 0) return <p>No matches, try a new search</p>
 
   return (
     <ul className="countries">
