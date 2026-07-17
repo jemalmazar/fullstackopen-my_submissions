bf6d5ad7790ca4249995d5d63a384ad50b90d51a
Jeremiah Almazar <jemalmazar@gmail.com>
2026-07-17 01:27:03 -0700

    uibuild

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 
 const app = express()
 
@@ -9,7 +8,7 @@ app.use(express.json())
 app.use(morgan(
   ':method :url :status :res[content-length] - :response-time ms :postData'
 ))
-app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('postData', (request, response) => {
   if (request.method === 'POST') {
