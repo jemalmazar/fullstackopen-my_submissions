d50afeaf5f8ff1515698f6dac058fd3c0339280c
Jeremiah Almazar <jemalmazar@gmail.com>
2026-07-16 20:50:37 -0700

    Add connection between part3/phonebook frontend and backend

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -8,6 +9,7 @@ app.use(express.json())
 app.use(morgan(
   ':method :url :status :res[content-length] - :response-time ms :postData'
 ))
+app.use(cors())
 
 morgan.token('postData', (request, response) => {
   if (request.method === 'POST') {
